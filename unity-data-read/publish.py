d30180dd8c6ed8cecc441a6d5bf1c3de65c892a9
mvandemheen892 <mvandemheen892@gmail.com>
2023-02-28 11:25:45 -0800

    modified

--- unity-data-read/publish.py
+++ unity-data-read/publish.py
@@ -2,8 +2,8 @@ import datetime
 import time
 
 while True:
-    f = open("demofile3.txt", "a")
-    now = str(datetime.datetime.now()) + '\n'
+    f = open("demofile3.txt", "w")
+    now = str(datetime.datetime.now())
     print(now)
     f.write(now)
     f.close()
