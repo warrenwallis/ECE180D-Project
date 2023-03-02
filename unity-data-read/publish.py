7efc5008e4ef9a6ee3041d196eec3b64ea079519
mvandemheen892 <mvandemheen892@gmail.com>
2023-03-02 10:06:03 -0800

    stuff

--- unity-data-read/publish.py
+++ unity-data-read/publish.py
@@ -1,11 +1,13 @@
 import datetime
 import time
+import random
 
 while True:
-    f = open("demofile3.txt", "w")
+    f = open("demofile3.txt", "a")
     now = str(datetime.datetime.now())
     print(now)
-    f.write(now)
+    f.write(now+"\n")
     f.close()
+    randomtime=random.randint(5,20)
     time.sleep(5)
 
